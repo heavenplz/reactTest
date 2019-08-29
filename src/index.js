19aa489f19c9f27252842f1ca7cc576c53985160
quan <luffy66@yeah.net>
2019-08-29 23:42:45 +0800

    在家改的

--- src/index.js
+++ src/index.js
@@ -61,44 +61,95 @@ import Hello from '@/componets/Hello'
 //     </div>,document.getElementById('app'))
 
 //类和对象的区别是什么
-function ObjectCat (name,age){
-    this.name = name;//通过new得到的实例，其属性叫做实例属性
-    this.age = age;
+// function ObjectCat (name,age){
+//     this.name = name;//通过new得到的实例，其属性叫做实例属性
+//     this.age = age;
+    
+// }
+
+
+// ObjectCat.info = "通过对象名访问到的属性是静态属性";
+
+// ObjectCat.prototype.meow = function (){
+//     console.log('通过prototype定义的方法是实例方法：通过访问静态方法的方式访问不到')
+// }
+
+// ObjectCat.Smeow = function(){
+//     console.log('通过对象名定义的方法是静态方法:通过访问实例方法的方式访问不到')
+// }
+
+
+// var oc = new ObjectCat('babe',18);
+
+
+// console.log("@@@"+ObjectCat.Smeow())
+
+
+// class Cat {
+//     constructor(name,age){
+//         this.name = name;//通过new得到的实例，其属性叫做实例属性
+//         this.age = age;
+//     }
+
+//     meow(){
+//         console.log("不加static关键字定义的方法是实例方法：不可以通过类名来访问");
+//     }
+
+//     static Smeow(){
+//         console.log("通过static关键字定义的方法是静态方法：通过实例名访问不到");
+//     }
+// }
+// var cc = new Cat('bb',28);
+
+// console.log(cc.meow())
+
+
+// class father{
+//     constructor(name,age){
+//         this.name = name;
+//         this.age = age;
+//     }
+//     sayHello(){
+//         console.log("大家好");
+//     }
+
+// }
+
+
+// class son extends father{
+
+// }
+
+// class daughter extends father{
+//     constructor(a,b){
+//         super(a,b);
+//     }
+
+// }
+
+// var s = new son('tom',8)
+
+// var d = new daughter('marry',12);
+
+// console.log(d.name,d.age);
+
+const attr ={
+    year:2017,
+    yh:10.2,
+    pl:'2.0t'
+}
+
+    
+
+class Car extends React.Component{
+    render(){
+        return <div>这是由Car继承自React渲染得到{this.props.pl}</div>
+    }
     
 }
 
+ReactDOM.render(<div>
 
-ObjectCat.info = "通过对象名访问到的属性是静态属性";
+   <Car {...attr}></Car> 
 
-ObjectCat.prototype.meow = function (){
-    console.log('通过prototype定义的方法是实例方法：通过访问静态方法的方式访问不到')
-}
-
-ObjectCat.Smeow = function(){
-    console.log('通过对象名定义的方法是静态方法:通过访问实例方法的方式访问不到')
-}
-
-
-var oc = new ObjectCat('babe',18);
-
-
-console.log("@@@"+ObjectCat.Smeow())
-
-
-class Cat {
-    constructor(name,age){
-        this.name = name;//通过new得到的实例，其属性叫做实例属性
-        this.age = age;
-    }
-
-    meow(){
-        console.log("不加static关键字定义的方法是实例方法：不可以通过类名来访问");
-    }
-
-    static Smeow(){
-        console.log("通过static关键字定义的方法是静态方法：通过实例名访问不到");
-    }
-}
-var cc = new Cat('bb',28);
-
-console.log(cc.meow())
+</div>,document.getElementById('app'))
